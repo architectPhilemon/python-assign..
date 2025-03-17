7f696e32cabb16210da71bee96b5836776cd313f
Philemon mwendwa maithya <mwendwaphilemon84@gmail.com>
2025-03-18 00:22:55 +0300

    Add files via upload

---
+++ pm python calc.py
@@ -0,0 +1,23 @@
+print("Hello, World")
+#Welcome to a fun calculator!
+#ask the user to input the first number
+#we're using 'float()'to make sure numbers can have dec
+# input("5:"))
+#ask the user to input the second number
+num2 = ("10")
+#let's compute the sum, difference, product, and quotient
+#add the two numbers
+sum_result = 5 + 10
+#subtract the second number from the first
+difference_result = 10 - 5
+#multiply the two numbers
+product_result = 5 * 10
+#divide the first number by the second
+#the user is being responsible and not dividing with zero for now
+quotient_result = 5 / 10
+#show the user what we got
+print(f"result of the two numbers:")
+print(f"sum: {sum_result}")
+print(f"difference: {difference_result}")
+print(f"product: {product_result}")
+print(f"quotient: {quotient_result}")
